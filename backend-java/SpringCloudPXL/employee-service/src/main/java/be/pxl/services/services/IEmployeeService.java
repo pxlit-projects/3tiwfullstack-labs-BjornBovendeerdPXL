b132abff972c329c20c856149c44f0761be63774
Bjorn Bovendeerd <bjorn.bovendeerd@student.pxl.be>
2024-10-12 12:13:03 +0200

    java backend lab 2 fix

--- backend-java/SpringCloudPXL/employee-service/src/main/java/be/pxl/services/services/IEmployeeService.java
+++
@@ -1,9 +0,0 @@
-package be.pxl.services.services;
-
-import be.pxl.services.domain.Employee;
-
-import java.util.List;
-
-public interface IEmployeeService {
-    List<Employee> getAllEmployees();
-}
