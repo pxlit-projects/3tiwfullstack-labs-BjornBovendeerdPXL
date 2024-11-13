e86eef5d52dc594a258d31672c5deb5b0c4a07ac
BjornBoverndeerdPXL <Bjorn.Bovendeerd@student.pxl.be>
2024-11-13 11:53:54 +0100

    lab3 af, discovery-service en alle clients setup klaar

---
+++ backend-java/SpringCloudPXL/discovery-service/src/main/java/be/pxl/service/DiscoveryServiceApplication.java
@@ -0,0 +1,10 @@
+package be.pxl.service;
+
+
+public class DiscoveryServiceApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
